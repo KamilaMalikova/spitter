149a4e1ef7dc9dca93ef8183647a82eddbc875a8
k.malikova <k.malikova@qiwi.com>
2021-09-04 19:36:51 +0300

    hsqldb

---
+++ src/test/java/com/example/dev/spitter/SpitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev.spitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
